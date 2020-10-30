f44c4c142fa87fae018cfe7a7bba1322cd66afde
Boyd <robboy@ceh.ac.uk>
2020-10-30 14:25:15 +0000

    Added main functions

---
+++ applyFilters.R
@@ -0,0 +1,99 @@
+#' \code{applyFilters} - Apply filters to occupancy model outputs.
+#' 
+#' @description This function can be used to subset the occupancy model
+#'              outputs based on number of records, priority/ pollinator status
+#'              and region. It works at the level of the taxonomic group so
+#'              must be applied across multiple groups if needed.
+#'
+#' @param roster String. A dataframe with columns: datPath, modPath, ver, indicator, region,
+#'               nSamps, minObs, write, outPath, clipBy, group (see \code{createRoster}). 
+#' 	  
+#' @return A dataframe with processed model outputs to be passed to \code{calcMSI}.
+#'         
+#' @export
+#' 
+
+applyFilters <- function(roster) {
+  
+  samp_post <- tempSampPost(indata = paste0(roster$modPath, roster$group, "/occmod_outputs/", roster$ver, "/"),
+                            output_path = NULL,
+                            REGION_IN_Q = paste0("psi.fs.r_", roster$region),
+                            sample_n = roster$nSamps,
+                            group_name = roster$group,
+                            combined_output = TRUE,
+                            #max_year_model = 2018,
+                            #min_year_model = 1970,
+                            write = FALSE)
+  
+  samp_post$species <- tolower(samp_post$species)
+  
+  meta <- extractMeta(inPath = paste0(roster$datPath, roster$group, "/", roster$ver, "/"),
+                      group = as.character(roster$group),
+                      outPath = roster$metaPath,
+                      write = F)
+  
+  meta[ ,1] <- tolower(meta$Species)
+  
+  if (roster$clipBy != "species") {
+    
+    meta$min_year_data_r_GB <- min(meta$min_year_data_r_GB)
+    
+    meta$max_year_data_r_GB <- max(meta$max_year_data_r_GB)
+    
+  }
+  
+  if (roster$indicator == "priority") {
+    
+    prio <- sampSubset("priority",
+                       inPath = roster$metaPath) 
+    
+    keep <- tolower(prio)
+    
+    drop <- NULL
+    
+  } else if (roster$indicator == "pollinators") {
+    
+    poll <- sampSubset("pollinators",
+                       inPath = roster$metaPath)
+    
+    pollDrop <- sampSubset("poll2Drop",
+                           inPath = roster$metaPath)
+    
+    keep <- tolower(poll)
+    
+    drop <- tolower(pollDrop)
+    
+  } else {
+    
+    keep <- NULL
+    
+    drop <- NULL
+    
+  }
+  
+  stacked_samps <- tempStackFilter(input = "memory",
+                                   dat = samp_post,
+                                   indata= NULL,
+                                   output_path=NULL, 
+                                   group_name=paste0(roster$indicator, roster$group), 
+                                   metadata=meta, 
+                                   region = roster$region,
+                                   minObs = roster$minObs, 
+                                   maxStartGap = 0, 
+                                   maxEndGap = 0,
+                                   maxMiddleGap = 10, 
+                                   keepSpecies = keep, 
+                                   removeSpecies = drop,
+                                   ClipFirst = TRUE, 
+                                   ClipLast = TRUE)
+  
+  if (roster$write == TRUE) {
+    
+    save(stacked_samps, file = paste0(roster$outPath, roster$group, "_", roster$indicator, 
+                                      "_", roster$region, ".rdata"))
+    
+  }
+  
+  return(stacked_samps)
+  
+}
---
+++ createRoster.R
@@ -0,0 +1,70 @@
+#' \code{createRoster} - Specify how to filter the occupancy model outputs.
+#' 
+#' @description This function can be used to specify the filters that will be applied to 
+#'              the occupancy model outputs using \code{applyFilters}. It works by creating a list
+#'              of 1-row dataframes with all the information needed for \code{applyFilters}. This list 
+#'              is then applied to the applyFilters function later. Arguments should be provided
+#'              as vectors of equal length, with each element in the vectors corresponding to one 
+#'              call to \code{applyFilters}. 
+#'              
+#' @param modPath String. Location of the occupancy model outputs. 
+#' 
+#' @param ver String. Which set of occupancy model outputs? E.g. Charlie's are "2017_Charlie".
+#' 
+#' @param indicator String. Whether or not to subset species and, if so, based
+#'                  on what. Options are: "priority" for priority species; 
+#'                  "pollinators" for pollinators; and all to return all species in the group.
+#'                  
+#' @param region String. One of "UK", "GB", "England", "Wales", "Scotland", 
+#'               or "NorthernIreland".
+#'
+#' @param nSamps Numeric. Number of samples to extract from each species' 
+#'               posterior distribution.
+#'               
+#' @param minObs Numeric. Threshold number of observation below which a 
+#'               species is dropped from the sample.
+#'               
+#' @param write Logical. If TRUE then the outputs are written as a 
+#'              .rdata file to outPath.
+#'              
+#' @param outPath String. Location to store the outputs if write = TRUE. 
+#'  	  
+#' @param clipBy String. One of "species" or "group" indicating whether to clip outputs by the first and last years of data for
+#'        each species or for the whole group, respectively.
+#' 	  
+#' @return A list of 1-row dataframes containing all arguments needed for applyFilters. applyFilters
+#'         can then be applied to this list to filter models outputs for different groups, which may 
+#'         come from different rounds (e.g. Charlie's or later), for different regions, etc. 
+#'         
+#' @export
+#' 
+
+createRoster <- function(modPath = "/data-s3/occmods/", 
+                         datPath,
+                         metaPath,
+                         ver = "2017_Charlie",
+                         group, 
+                         indicator, 
+                         region,
+                         nSamps = 1000,
+                         minObs = 50,
+                         write,
+                         outPath,
+                         clipBy = "species") {
+  
+  df <- data.frame(modPath = modPath,
+                   datPath = datPath,
+                   metaPath = metaPath,
+                   ver = ver, 
+                   group = group, 
+                   indicator = indicator,
+                   region = region, 
+                   nSamps = nSamps, 
+                   minObs = minObs, 
+                   write = write, 
+                   outPath = outPath,
+                   clipBy = clipBy)
+  
+  roster <- split(df, seq(nrow(df)))
+  
+}
